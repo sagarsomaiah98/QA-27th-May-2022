fd4f6334784cb263ce071bc980d8a660850c3e79
Som <somaiah1988@gmail.com>
2022-06-17 20:58:13 -0400

    first commit

---
+++ P2/src/T1.java
@@ -0,0 +1,14 @@
+
+public class T1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I am learning Java.");
+		System.out.println("It is awesome!");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
