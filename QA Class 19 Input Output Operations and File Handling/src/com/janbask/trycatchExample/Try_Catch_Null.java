729c34c204bf2ec0086447525f8773c32de477b9
Som <somaiah1988@gmail.com>
2022-06-27 22:22:15 -0400

    27

--- QA Class 19 Input Output Operations and File Handling/src/com/janbask/trycatchExample/Try_Catch_Null.java
+++ QA Class 19 Input Output Operations and File Handling/src/com/janbask/trycatchExample/Try_Catch_Null.java
@@ -3,10 +3,17 @@ public class Try_Catch_Null {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int h=10;
 
 		try {
 			
-			int h=10/1;
+			System.out.println("in the try block");
+			
+			System.out.println(h);
+			 h=20/0;
+			 System.out.println(" value of h");
+			 System.out.println(h);
 			
 		}
 		catch(Exception v)
@@ -17,6 +24,9 @@ public class Try_Catch_Null {
 			System.out.println(" cannot divide 10 by 0");
 			
 		}
+		
+		System.out.println(" end of the program");
+		
 		
 	}
 
