b8eb4aaed68e5a2faaa0313543f65c072f6d3c8c
Som <somaiah1988@gmail.com>
2022-07-12 22:11:03 -0400

    08

---
+++ SampleMvn/src/test/java/LoginTest.java
@@ -0,0 +1,27 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+		driver.findElement(By.xpath("(//input[@id='btnLogin'])[1]")).click();
+		Thread.sleep(2000);
+		
+		driver.quit();
+	}
+
+}
